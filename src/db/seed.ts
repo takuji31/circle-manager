11f30ba79d8f4120a0b9fc0c76cab932c72398bf
Takuji Nishibayashi <takuji31@gmail.com>
2021-10-02 01:22:15 +0900

    add seed script

---
+++ src/db/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const now = new Date();
+  prisma.circle.createMany({
+    skipDuplicates: true,
+    data: [
+      {
+        id: "863398236474834944",
+        name: "シン・ウマ娘愛好会",
+        createdAt: new Date(now.getTime() - 2000),
+      },
+      {
+        id: "870950796479594556",
+        name: "ウマ娘新愛好会：破",
+        createdAt: new Date(now.getTime() - 1000),
+      },
+      {
+        id: "863398725920227339",
+        name: "ウマ娘新愛好会：序",
+        createdAt: now,
+      },
+    ],
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
