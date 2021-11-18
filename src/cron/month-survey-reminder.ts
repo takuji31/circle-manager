63d8d0a8a2822ac6073f3976f282d80ee652f30c
Takuji Nishibayashi <takuji31@gmail.com>
2021-11-18 17:54:51 +0900

    Update month-survey-reminder.ts

--- src/cron/month-survey-reminder.ts
+++ src/cron/month-survey-reminder.ts
@@ -3,6 +3,7 @@ import { nextMonth } from './../model/year_month';
 import { prisma } from './../database/prisma';
 import { config } from 'dotenv';
 import { sendDirectMessagesIfPossible } from '../discord/message';
+import { Temporal } from 'proposal-temporal';
 
 config();
 
@@ -34,17 +35,36 @@ config();
       ],
     },
   });
+  const monthSurvey = await prisma.monthSurvey.findUnique({
+    where: { year_month: month },
+  });
+  if (!monthSurvey) {
+    return;
+  }
   const messages: Array<string> = [];
+  const expiredAt = Temporal.Instant.fromEpochMilliseconds(
+    monthSurvey.expiredAt.getTime()
+  )
+    .toZonedDateTime({
+      timeZone: 'Asia/Tokyo',
+      calendar: 'iso8601',
+    })
+    .toLocaleString('ja-JP', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      weekday: 'short',
+      hour: 'numeric',
+    });
   await sendDirectMessagesIfPossible(
     members,
     () => {
-      return `在籍希望アンケートが未回答です。期限までに必ず`;
+      return `※このメッセージは自動送信です。\n\n在籍希望アンケートが未回答です。期限(${expiredAt})までに必ず回答してください。回答がない場合は除名となります！\n\nよろしくお願いします。`;
     },
     (
       `在籍希望アンケートの未回答者へのリマインダー送信結果 ${
         !isProduction ? '(テスト)' : ''
       }\n    ` + messages.join('\n    ')
-    ).trim(),
-    false
+    ).trim()
   );
 })();
