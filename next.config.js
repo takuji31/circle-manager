29af89a35c1ced06681f12fc64a12ccc92e6f9fd
Takuji Nishibayashi <takuji31@gmail.com>
2021-12-06 00:09:23 +0900

    Update Next to 12

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const { withSuperjson } = require('next-superjson');
+
 /** @type {import('next').NextConfig} */
+// module.exports = withSuperjson()({ reactStrictMode: true });
 module.exports = {
   reactStrictMode: true,
-}
+};
