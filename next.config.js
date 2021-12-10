52063751740393c5a261f0b0905e4311e376893d
Takuji Nishibayashi <takuji31@gmail.com>
2021-12-10 21:50:31 +0900

    remove superjson

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-const { withSuperjson } = require('next-superjson');
-
 /** @type {import('next').NextConfig} */
-// module.exports = withSuperjson()({ reactStrictMode: true });
 module.exports = {
   reactStrictMode: true,
 };
