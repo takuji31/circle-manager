b0573a1eedda1d056fd12815b0276ba1dead8d05
Takuji Nishibayashi <takuji31@gmail.com>
2022-01-25 23:34:32 +0900

    MonthCircle currentCircleKeyはNULLのままでよい

--- prisma/migrations/20220125141826_month_circle_circle_key_not_null/migration.sql
+++ prisma/migrations/20220125141826_month_circle_circle_key_not_null/migration.sql
@@ -1,10 +1,2 @@
-/*
-  Warnings:
-
-  - Made the column `currentCircleKey` on table `MonthCircle` required. This step will fail if there are existing NULL values in that column.
-  - Made the column `state` on table `MonthCircle` required. This step will fail if there are existing NULL values in that column.
-
-*/
 -- AlterTable
-ALTER TABLE "MonthCircle" ALTER COLUMN "currentCircleKey" SET NOT NULL,
-ALTER COLUMN "state" SET NOT NULL;
+ALTER TABLE "MonthCircle" ALTER COLUMN "state" SET NOT NULL;
