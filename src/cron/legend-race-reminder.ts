e66546c525569915c521e989bd0e1110321b6b18
Takuji Nishibayashi <takuji31@gmail.com>
2022-03-11 21:17:36 +0900

    レジェンドレースリマインダーのCronスクリプトを追加
    #55

---
+++ src/cron/legend-race-reminder.ts
@@ -0,0 +1,22 @@
+import { config } from 'dotenv';
+import { Guild } from '../model';
+import { createDiscordRestClient } from '../discord';
+import {
+  AllowedMentionsTypes,
+  RESTPostAPIChannelMessageJSONBody,
+  Routes,
+} from 'discord-api-types/v9';
+
+config();
+
+(async () => {
+  const rest = createDiscordRestClient();
+
+  const body: RESTPostAPIChannelMessageJSONBody = {
+    content: `@here レジェンドレースやった？`,
+    allowed_mentions: {
+      parse: ['everyone' as AllowedMentionsTypes],
+    },
+  };
+  await rest.post(Routes.channelMessages(Guild.channelIds.random), { body });
+})();
