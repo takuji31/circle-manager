c000375ac5857634df80ced26f15b56f5a55499e
Takuji Nishibayashi <takuji31@gmail.com>
2022-03-18 20:11:38 +0900

    加入申請画面で序を選べないようにする
    #52

--- src/graphql/fields/circle.ts
+++ src/graphql/fields/circle.ts
@@ -1,16 +1,10 @@
 import { list, nonNull, queryField } from 'nexus';
 import { Circle as C } from '../../model';
-import { Circle, CircleFilter } from '../types/';
+import { Circle } from '../types/';
 
 export const CirclesQueryField = queryField('circles', {
   type: nonNull(list(nonNull(Circle))),
-  args: {
-    filter: CircleFilter.asArg({ default: 'All' }),
-  },
-  resolve(_, { filter: f }, ctx) {
-    // TODO: fix
-    const circleSelect = f == 'CircleSelect';
-    const monthSurvey = f == 'MonthSurvey';
-    return [C.saikyo, C.shin, C.jo, C.ha];
+  resolve(_) {
+    return [C.saikyo, C.shin, C.ha];
   },
 });
