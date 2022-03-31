7a020c8f95815fd05cdaf28d43318cb4cb30f4b1
Takuji Nishibayashi <takuji31@gmail.com>
2022-03-31 16:19:56 +0900

    enable streaming feature and minify

--- v2/packages/web/next.config.js
+++ v2/packages/web/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    runtime: 'nodejs',
+  },
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
