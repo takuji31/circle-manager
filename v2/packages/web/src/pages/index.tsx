305af1b7110b716f72fd46643ec377847aafbaea
Takuji Nishibayashi <takuji31@gmail.com>
2022-04-13 23:48:13 +0900

    remove v2

--- v2/packages/web/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next';
-import Layout from '../components/layout';
-
-const Home: NextPage = () => {
-  return <Layout title="Top"></Layout>;
-};
-
-export default Home;
