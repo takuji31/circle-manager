f537e30c7bf6ce414fad6d1b2494ef90559820c5
Takuji Nishibayashi <takuji31@gmail.com>
2022-04-19 20:33:36 +0900

    管理画面を全てSSGできるようにするために/admin/にトップの機能を移動して、管理者は管理用トップにリダイレクトする

---
+++ packages/web/src/pages/_middleware.ts
@@ -0,0 +1,20 @@
+import { JWT } from 'next-auth/jwt';
+import { withAuth } from 'next-auth/middleware';
+import { NextMiddleware, NextRequest, NextResponse } from 'next/server';
+
+const middleware: NextMiddleware = ((
+  req: NextRequest & { nextauth: { token: JWT } }
+) => {
+  const url = req.nextUrl.clone();
+  if (url.pathname == '/' && req.nextauth.token.isAdmin) {
+    url.pathname = '/admin';
+    return NextResponse.redirect(url);
+  }
+  return undefined;
+}) as unknown as NextMiddleware;
+
+export default withAuth(middleware, {
+  callbacks: {
+    authorized: ({ req, token }) => !!token,
+  },
+});
