dbf3f63d55b6d1237ba32b3a19530197947aa058
Takuji Nishibayashi <takuji31@gmail.com>
2022-04-29 02:39:34 +0900

    ネストしたメニューが表示されるようにする

--- packages/web2/app/root.tsx
+++ packages/web2/app/root.tsx
@@ -91,14 +91,12 @@ const adminNavigations: Array<NavItem | NestedNavItem> = [
   { name: "トップ", href: "/" },
   { name: "メンバー一覧", href: "/admin/members" },
   {
-    name: "サークル一覧",
+    name: "サークル",
     children: Circles.activeCircles.map((circle) => ({
       name: circle.name,
       href: `/circles/${circle.key}`,
     })),
   },
-  { name: "Projects", href: "#" },
-  { name: "Calendar", href: "#" },
 ];
 
 function isNestedNavItem(
@@ -117,7 +115,7 @@ const Navigation: React.FC = () => {
     >
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="w-full px-2 sm:px-4">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
@@ -202,10 +200,10 @@ const Navigation: React.FC = () => {
                               method="post"
                               className={classNames(
                                 active ? "bg-gray-100 dark:bg-neutral-600" : "",
-                                "block px-4 py-2 text-sm text-neutral-900 dark:text-neutral-100"
+                                "block w-full text-sm text-neutral-900 dark:text-neutral-100"
                               )}
                             >
-                              <button className="block h-full w-full text-left">
+                              <button className="block h-full w-full px-4 py-2  text-left">
                                 ログアウト
                               </button>
                             </Form>
@@ -285,20 +283,26 @@ const NestedNavItemLink: React.FC<{
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="px-1 py-1 ">
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  className={`${
-                    active ? "bg-violet-500 text-white" : "text-gray-900"
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                >
-                  Edit
-                </button>
-              )}
-            </Menu.Item>
-          </div>
+        <Menu.Items className="absolute left-0 mt-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-neutral-700">
+          {item.children.map((child, idx) => {
+            return (
+              <div key={idx}>
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link
+                      to={child.href}
+                      className={classNames(
+                        active ? "bg-neutral-200 dark:bg-neutral-600" : "",
+                        "group flex w-full items-center rounded-md px-4 py-2 text-base  text-white"
+                      )}
+                    >
+                      {child.name}
+                    </Link>
+                  )}
+                </Menu.Item>
+              </div>
+            );
+          })}
         </Menu.Items>
       </Transition>
     </Menu>
