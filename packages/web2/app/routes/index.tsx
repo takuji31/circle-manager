0da4e76bcf35cda8f2a744182c717da495965a93
Takuji Nishibayashi <takuji31@gmail.com>
2022-04-29 02:39:22 +0900

    ログイン状態に応じてメッセージを出す

--- packages/web2/app/routes/index.tsx
+++ packages/web2/app/routes/index.tsx
@@ -1,3 +1,20 @@
+import { use } from "@circle-manager/shared/model";
+import { useOptionalUser } from "~/utils";
+
 export default function Index() {
-  return <div style={{ height: "100000px" }}>hogehoge</div>;
+  const user = useOptionalUser();
+  if (!user) {
+    return <p className="text-base">ログインしてください</p>;
+  } else if (!user.isMember) {
+    return (
+      <p className="text-base">
+        サークルに加入されていません、加入については運営メンバーへお問い合わせください。
+      </p>
+    );
+  } else if (!user.isAdmin) {
+    return <p className="text-base">メンバー向けのページは準備中です。</p>;
+  }
+  return (
+    <p className="block p-4 text-base">上部のメニューを選択してください。</p>
+  );
 }
