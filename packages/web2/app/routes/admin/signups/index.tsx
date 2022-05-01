261b2cc215792edb90e81685c80c97084f0c5db0
Takuji Nishibayashi <takuji31@gmail.com>
2022-05-02 05:19:58 +0900

    加入申請が一通り動くように

--- packages/web2/app/routes/admin/signups/index.tsx
+++ packages/web2/app/routes/admin/signups/index.tsx
@@ -16,7 +16,11 @@ import React, {
 import { Dialog, Switch, Transition } from "@headlessui/react";
 import { classNames } from "~/lib";
 import Card from "~/components/card";
-import { getNotJoinedSignUps, inviteMember } from "~/model/signup.server";
+import {
+  getNotJoinedSignUps,
+  inviteMember,
+  joinMember,
+} from "~/model/signup.server";
 import { useUser } from "~/utils";
 import { ClipboardCopyIcon, ExclamationIcon } from "@heroicons/react/outline";
 import CopyToClipboard from "react-copy-to-clipboard";
@@ -43,11 +47,16 @@ export const action: ActionFunction = adminOnlyAction(async ({ request }) => {
   const formData = await request.formData();
   const memberId = formData.get("memberId") as string;
   const invited = !!formData.get("invited");
+  const joined = !!formData.get("joined");
 
   invariant(memberId, "memberId cannot null");
 
   if (invited) {
     await inviteMember({ memberId });
+  }
+
+  if (joined) {
+    await joinMember({ memberId });
   }
   return null;
 });
@@ -125,7 +134,7 @@ export default function AdminSignUps() {
           {notInvitedSignUps.length ? (
             <SignUpList
               children={notInvitedSignUps.map((signUp) => (
-                <SignUpListItem key={signUp.id} signUp={signUp} />
+                <NotInvitedListItem key={signUp.id} signUp={signUp} />
               ))}
             />
           ) : (
@@ -144,7 +153,7 @@ export default function AdminSignUps() {
           {invitedSignUps.length ? (
             <SignUpList
               children={invitedSignUps.map((signUp) => (
-                <SignUpListItem key={signUp.id} signUp={signUp} />
+                <InvitedListItem key={signUp.id} signUp={signUp} />
               ))}
             />
           ) : (
@@ -166,7 +175,127 @@ const SignUpList: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   );
 };
 
-const SignUpListItem: React.FC<{ signUp: SignUp }> = ({ signUp }) => {
+const InvitedListItem: React.FC<{ signUp: SignUp }> = ({ signUp }) => {
+  const [open, setOpen] = useState(false);
+  const transition = useTransition();
+  const cancelButtonRef = useRef(null);
+  return (
+    <li className="py-4">
+      <div className="flex items-center space-x-4">
+        <div className="min-w-0 flex-1">
+          <p className="truncate text-sm font-medium text-gray-900">
+            {signUp.member.name}
+          </p>
+          <p className="truncate text-sm text-gray-500">
+            {signUp.circle?.name ?? "未選択"}
+          </p>
+        </div>
+        <div className="flex flex-row items-center space-x-2">
+          <Form method="post">
+            <input name="memberId" type="hidden" value={signUp.member.id} />
+            <button
+              disabled={transition.state == "submitting"}
+              className="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={() => setOpen(true)}
+            >
+              加入済みにする
+            </button>
+          </Form>
+        </div>
+        <Transition.Root show={open} as={Fragment}>
+          <Dialog
+            as="div"
+            className="fixed inset-0 z-10 overflow-y-auto"
+            initialFocus={cancelButtonRef}
+            onClose={() => setOpen(false)}
+          >
+            <div className="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+              </Transition.Child>
+
+              {/* This element is to trick the browser into centering the modal contents. */}
+              <span
+                className="hidden sm:inline-block sm:h-screen sm:align-middle"
+                aria-hidden="true"
+              >
+                &#8203;
+              </span>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <div className="relative inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:align-middle">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg font-medium leading-6 text-gray-900"
+                      >
+                        加入済みにします
+                      </Dialog.Title>
+                      <div className="mt-2 text-sm text-gray-500">
+                        <p>
+                          加入済みにしてメンバーのロールを更新します、実行するとリストから消えますので必ず加入が済んだことを確認してください。
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                    <Form method="post">
+                      <input
+                        name="memberId"
+                        type="hidden"
+                        value={signUp.member.id}
+                      />
+                      <input name="joined" type="hidden" value="joined" />
+                      <button
+                        type="submit"
+                        className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                        onClick={() => setOpen(false)}
+                      >
+                        加入済みにする
+                      </button>
+                    </Form>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
+                      onClick={() => setOpen(false)}
+                      ref={cancelButtonRef}
+                    >
+                      キャンセル
+                    </button>
+                  </div>
+                </div>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
+      </div>
+    </li>
+  );
+};
+
+const NotInvitedListItem: React.FC<{ signUp: SignUp }> = ({ signUp }) => {
   const [open, setOpen] = useState(false);
   const [copied, setCopied] = useState(false);
   const transition = useTransition();
