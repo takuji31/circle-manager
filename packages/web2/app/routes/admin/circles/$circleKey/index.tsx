4f244a0fb9e583dfebdd384ba3a7cf563b213836
Takuji Nishibayashi <takuji31@gmail.com>
2022-05-13 13:52:15 +0900

    サークルの詳細画面をMUIにする

--- packages/web2/app/routes/admin/circles/$circleKey/index.tsx
+++ packages/web2/app/routes/admin/circles/$circleKey/index.tsx
@@ -1,16 +1,18 @@
-import AdminHeader from "~/components/admin/header";
-import AdminHeaderTitle from "~/components/admin/header/title";
-import React from "react";
 import type { LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "remix";
+import { Link, useLoaderData } from "remix";
 import type { Params } from "react-router";
 import { z } from "zod";
 import { ActiveCircleKey } from "~/schema/member";
 import { notFound } from "~/response.server";
 import { Circles, LocalDate, Period } from "@circle-manager/shared/model";
-import Link from "~/components/link";
 import { getDatesFrom } from "~/model/date.server";
-import { AdminBody } from "~/components/admin/body";
+import { Card, CardHeader } from "@mui/material";
+import { Grid } from "@mui/material";
+import { Paper } from "@mui/material";
+import { Typography } from "@mui/material";
+import { CardActionArea } from "@mui/material";
+import { CardContent } from "@mui/material";
+import { Box } from "@mui/system";
 
 type LoaderData = Awaited<ReturnType<typeof getLoaderData>>;
 
@@ -40,39 +42,34 @@ export const loader: LoaderFunction = async ({ params }) => {
 export default function AdminCirclesCircleKey() {
   const { circle, dates } = useLoaderData<LoaderData>();
   return (
-    <div>
-      <AdminHeader>
-        <AdminHeaderTitle title={circle.name} />
-      </AdminHeader>
-      <AdminBody>
-        <div className="border-b border-gray-200 pb-5">
-          <h2 className="text-lg font-medium leading-6 text-gray-900">
-            ファン数記録・確認
-          </h2>
-        </div>
-        <ul
-          role="list"
-          className="mt-3 grid grid-cols-2 gap-2 sm:grid-cols-4 sm:gap-4 lg:grid-cols-6"
-        >
+    <Box p={2}>
+      <Card>
+        <CardHeader title="ファン数記録・確認" />
+        <Grid container spacing={2} mt={3}>
           {dates.map((date) => (
-            <li
+            <Grid
+              item
               key={`${date.year}${date.month}${date.day}`}
-              className="col-span-1 flex rounded-md shadow-sm"
+              xs={6}
+              sm={3}
+              lg={2}
             >
-              <div className="flex flex-1 items-center justify-between truncate rounded-md border border-gray-200 bg-white">
-                <Link
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea
+                  component={Link}
                   to={`/admin/circles/${circle.key}/fans/${date.year}/${date.month}/${date.day}`}
-                  className="group flex-1 truncate px-4 py-4 text-sm"
                 >
-                  <span className="font-medium text-gray-900 group-hover:text-gray-600">
-                    {`${date.year}/${date.month}/${date.day}`}
-                  </span>
-                </Link>
-              </div>
-            </li>
+                  <CardContent>
+                    <Typography variant="body1">
+                      {`${date.year}/${date.month}/${date.day}`}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
           ))}
-        </ul>
-      </AdminBody>
-    </div>
+        </Grid>
+      </Card>
+    </Box>
   );
 }
