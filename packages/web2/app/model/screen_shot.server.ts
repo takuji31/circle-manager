0a49b4c3b38989383ac37e97c84b5ecb6282f84e
Takuji Nishibayashi <takuji31@gmail.com>
2022-05-13 17:30:51 +0900

    Update screen_shot.server.ts

--- packages/web2/app/model/screen_shot.server.ts
+++ packages/web2/app/model/screen_shot.server.ts
@@ -242,7 +242,7 @@ const parseScreenShot = async ({
 
   // TODO: ユニット数節約のために一度パースしたスクリーンショットは再度Cloud Vision APIに投げない
   const [result] = await client.documentTextDetection(
-    `gs://${file.bucket.name}/${file.name}`
+    `gs://${file.bucket.name}${file.name}`
   );
   const annotations = result.fullTextAnnotation;
 
