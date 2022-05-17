440916244c676a73d5b83ae7d37148bf12a4bc63
Takuji Nishibayashi <takuji31@gmail.com>
2022-05-17 09:30:54 +0900

    sharedとweb2のコードをルートに移動

--- copy-production-data-to-devel.sh
+++ copy-production-data-to-devel.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-DATABASE_URL='mysql://root:@127.0.0.1:3309/shin-umamusume' yarn w ts-node src/tools/export-db-data.ts
-DATABASE_URL='mysql://root:@127.0.0.1:3306/shin-umamusume-dev' yarn w ts-node src/tools/import-db-data.ts
+DATABASE_URL='mysql://root:@127.0.0.1:3309/shin-umamusume' yarn ts-node tools/export-db-data.ts
+DATABASE_URL='mysql://root:@127.0.0.1:3306/shin-umamusume-dev' yarn ts-node tools/import-db-data.ts
