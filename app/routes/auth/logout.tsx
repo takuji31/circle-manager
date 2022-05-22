8561b4822608a9d6b1aa17c9a5d6b6cac8c21b81
Takuji Nishibayashi <takuji31@gmail.com>
2022-05-22 15:56:50 +0900

    Discordのログイン時に完了後のリダイレクト先を指定できるようにした

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -2,5 +2,10 @@ import type { ActionFunction } from "@remix-run/node";
 import { authenticator } from "~/auth.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  await authenticator.logout(request, { redirectTo: "/" });
+  const url = new URL(request.url);
+  const returnTo = url.searchParams.get("returnTo");
+  if (returnTo && !returnTo.match(/^\//)) {
+    throw new Response("Invalid return url", { status: 400 });
+  }
+  await authenticator.logout(request, { redirectTo: returnTo ?? "/" });
 };
