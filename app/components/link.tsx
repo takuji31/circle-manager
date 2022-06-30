3015f3c477a8d679c19ce42ebb1b99e1b9576ff0
Takuji Nishibayashi <takuji31@gmail.com>
2022-06-30 15:47:44 +0900

    fix import

--- app/components/link.tsx
+++ app/components/link.tsx
@@ -1,16 +1,14 @@
-import type { RemixLinkProps } from "@remix-run/react/components";
 import { Link as RemixLink } from "@remix-run/react";
+import type { RemixLinkProps } from "@remix-run/react/dist/components";
 import { classNames } from "~/lib";
 
-const Link: React.FC<
-  { children: React.ReactNode } & RemixLinkProps &
-    React.RefAttributes<HTMLAnchorElement>
-> = ({ children, className, ...props }) => {
+const Link: React.FC<{ children: React.ReactNode } & RemixLinkProps &
+  React.RefAttributes<HTMLAnchorElement>> = ({ children, className, ...props }) => {
   return (
     <RemixLink
       className={classNames(
         "text-indigo-600 hover:text-indigo-900",
-        className ?? ""
+        className ?? "",
       )}
       {...props}
     >
